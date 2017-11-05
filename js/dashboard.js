636df4fc23cb574edd3492006629529df7db30c1
Chad Conway <agile4java@gmail.com>
2017-11-05 16:27:47 -0600

    Initial Commit: Web Visualization Frameworks

---
+++ js/dashboard.js
@@ -0,0 +1,19 @@
+var kpi1Data;
+var kpi2Data;
+var kpi3Data;
+var kpi4Data;
+var kpi1;
+
+
+window.onload = function() {
+    kpi1Data = [[[1, 2.1], [2, 2.75], [3, 5.6], [4, 4.9]]];
+    kpi2Data = [[[1, 1.3], [2, 1.4], [3, 1.4], [4, 1.3]]];
+    kpi3Data =  [[[1, 0.8], [2, 1.35], [3, 4.2], [4, 3.6]]];
+    kpi4Data = [[[1, 75], [2, 83], [3, 150], [4, 130]]];
+
+};
+
+$(document).ready(function(){
+    $("#brander").text("jQuery is Working!!!!");
+
+})
---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+
+
+function clearKPIs(){}
+function flotr2(){
+
+    var kpi1 = document.getElementById("kpi1");
+    Flotr.draw(kpi1, kpi1Data);
+    var kpi2 = document.getElementById("kpi2");
+    Flotr.draw(kpi2, kpi2Data);
+    var kpi3 = document.getElementById("kpi3");
+    Flotr.draw(kpi3, kpi3Data);
+    var kpi4 = document.getElementById("kpi4");
+    Flotr.draw(kpi4, kpi4Data);
+}
+function chartjs(){}
+function chartist(){}
+function googleCharts(){}
+function d3(){}
+function leaflet(){}
