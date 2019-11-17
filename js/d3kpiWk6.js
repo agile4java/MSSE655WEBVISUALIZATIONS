695e84219fe4a4f6bb15943e3f2e5a806395fefb
Chad Conway <agile4java@gmail.com>
2019-11-17 14:20:56 -0600

    updates

--- js/d3kpiWk6.js
+++ js/d3kpiWk6.js
@@ -1,18 +1,21 @@
-function d3Draw(){
-    clearKPIs();
+function d3Draw() {
+	clearKPIs();
+	clearKPIs();
+	$('#page-header').text('d3 - Data Driven Documents');
 
-    var data4d3 = [25, 20, 40, 45, 60, 65, 75, 80];
+	var data4d3 = [25, 20, 40, 45, 60, 65, 75, 80];
 
-
-
-
-    d3.select("#kpi4")
-        .selectAll("div")
-        .data(data4d3)
-        .enter()
-        .append("div")
-        .attr("id", "d3Bar")
-        .style("width", function(d) {return (d) + "vw";})
-        .style("height", "3vh")
-        .text(function(d){return d;});
+	d3.select('#kpi4')
+		.selectAll('div')
+		.data(data4d3)
+		.enter()
+		.append('div')
+		.attr('id', 'd3Bar')
+		.style('width', function(d) {
+			return d + 'vw';
+		})
+		.style('height', '3vh')
+		.text(function(d) {
+			return d;
+		});
 }
--- js/dashboard.js
+++ js/dashboard.js
@@ -4,17 +4,42 @@ var kpi3Data;
 var kpi4Data;
 var kpi1;
 
-
 window.onload = function() {
-    kpi1Data = [[[1, 2.1], [2, 2.75], [3, 5.6], [4, 4.9]]]; //revenue
-    kpi2Data = [[[1, 1.3], [2, 1.4], [3, 1.4], [4, 1.3]]]; //expenses
-    kpi3Data =  [[[1, 0.8], [2, 1.35], [3, 4.2], [4, 3.6]]]; //profit
-    kpi4Data = [[[1, 75], [2, 83], [3, 150], [4, 130]]]; //customers
-
-
+	kpi1Data = [
+		[
+			[1, 2.1],
+			[2, 2.75],
+			[3, 5.6],
+			[4, 4.9]
+		]
+	]; //revenue
+	kpi2Data = [
+		[
+			[1, 1.3],
+			[2, 1.4],
+			[3, 1.4],
+			[4, 1.3]
+		]
+	]; //expenses
+	kpi3Data = [
+		[
+			[1, 0.8],
+			[2, 1.35],
+			[3, 4.2],
+			[4, 3.6]
+		]
+	]; //profit
+	kpi4Data = [
+		[
+			[1, 75],
+			[2, 83],
+			[3, 150],
+			[4, 130]
+		]
+	]; //customers
+	flotr2();
 };
 
-$(document).ready(function(){
-    $("#brander").text("jQuery is Working!!!!");
-
-})
+$(document).ready(function() {
+	$('#brander').text('Web Visualizations by Plugin:');
+});
